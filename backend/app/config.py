2f6b816aae4bba33e67afdd359fec483941c9370
Mariana Bll <100840719+marianabll@users.noreply.github.com>
2024-09-26 19:28:13 -0300

    Delete backend/app/config.py

--- backend/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    """Classe de configuração para a aplicação."""
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default_secret_key'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/seu_banco_de_dados'
-
-# SECRET_KEY: Uma chave secreta usada para proteger dados da sessão. Pode ser definida através de uma variável de ambiente ou uma chave padrão.
-# MONGO_URI: A URI de conexão com o MongoDB, que pode ser configurada através de uma variável de ambiente ou um valor padrão.
