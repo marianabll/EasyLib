e69cb3b851abc33bf4a212e661679857279a10fe
Mariana Bll <100840719+marianabll@users.noreply.github.com>
2024-10-18 17:11:25 -0300

    Update db_connection.py

--- backend/app/db_connection.py
+++ backend/app/db_connection.py
@@ -8,16 +8,13 @@ load_dotenv()
 # Obter a URI do MongoDB a partir da variável de ambiente
 MONGODB_URI = os.getenv("MONGODB_URI")
 
-# Verificar se a URI foi carregada corretamente
-if not MONGODB_URI:
-    raise ValueError("A URI do MongoDB não foi encontrada. Verifique seu arquivo .env.")
-
-# Conectar ao MongoDB
+# Criar a conexão com o MongoDB Atlas
 client = MongoClient(MONGODB_URI)
 
-# Acessar o banco de dados
+# Acessar o banco de dados (nome do seu banco)
 db = client['Library24']
 
 # Acessar as coleções
 users_collection = db['Users']
 books_collection = db['Books']
+
